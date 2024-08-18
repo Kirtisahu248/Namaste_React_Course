68703044156f0711cddf1b94863c624007fa58ba
Kirti Sahu <sahukirti1804@gmail.com>
2024-08-18 23:41:56 +0530

    Initial commit

--- Chapter-02_Igniting_Our_App/README.md
+++ Chapter-02_Igniting_Our_App/README.md
@@ -1,1 +1,20 @@
-#Namaste React
+# Namaste React
+
+# Parcel
+- DevBuild
+-Local Server
+-HMR = Hot Module Replacement
+-File watching algorithm = written in c++
+-Faster build - because of caching
+-Image Optimization
+-Minification of file
+-Bundling
+-Compress File
+-Consistent hashing
+-Code spliting
+-Differential Bundling - to support older browsers
+-Diagnostic
+-Error Handling
+-We can also host it in https (i.e. npx parcel index.html --https)
+-Tree Shaking - remove unused code for us
+-Different dev and production bundles
--- Chapter-02_Igniting_Our_App/app.js
+++ Chapter-02_Igniting_Our_App/app.js
@@ -1,20 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/* nested Element  example 
-*  <div id:"parent">
-*    <div id:"child">
-*       <h1>I am an h1 tag</h1>
-*       <h2>I am an h2 tag</h2>  // sibling
-*      </div>
-*
-*      <div id:"child2">
-*       <h1>I am an h1 tag</h1>
-*       <h2>I am an h2 tag</h2>  // sibling
-*      </div>
-*  </div>
-*   ReactElement(Object) => HTML(browser understand)
-*/
-
-//Lets create nested element in react
 const parent = React.createElement
     ( "div",
       { id: "parent" },
@@ -28,12 +14,6 @@ const parent = React.createElement
             ])
         ]
     );
-//const heading = React.createElement
-    //what is heading here? ->Basically it is an object (i.e. the object is  a react element of type 'h1')
-    /*("h1",
-        { id: "heading", xyz: "acb" },
-        "Hello World From React!");  // this argument is called children
-     */
 
 console.log(parent); // it will return object 
 
