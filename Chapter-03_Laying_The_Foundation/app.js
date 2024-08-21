6874d430730fd5f5cb385d9322a1f7057d19d908
Kirti Sahu <sahukirti1804@gmail.com>
2024-08-21 22:31:54 +0530

    Initial commit

--- Chapter-03_Laying_The_Foundation/app.js
+++ Chapter-03_Laying_The_Foundation/app.js
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom/client";
 
 //JSX - its like HTML Syntax
 const jsxheading = <h1 id="heading">Namaste React🚀</h1>;
+console.log(jsxheading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
