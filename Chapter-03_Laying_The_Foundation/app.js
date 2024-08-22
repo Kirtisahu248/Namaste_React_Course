5f994a7f1e0e6b17cf2ad1b7636e8cb13f697bcd
Kirti Sahu <sahukirti1804@gmail.com>
2024-08-22 23:00:53 +0530

    Initial commit

--- Chapter-03_Laying_The_Foundation/app.js
+++ Chapter-03_Laying_The_Foundation/app.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX - its like HTML Syntax
-const jsxheading = <h1 id="heading">Namaste React🚀</h1>;
+// React.createElement => Object =>HTMLElement(render)
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
+console.log(heading);
+
+//JSX - JSX is a javascript syntax (HTML -Like or XML-Like syntax)
+
+//JSX => create.ReactElement => ReactElement (JS Object) => HTMLElement(render)
+
+//JSX code is transpiled before it reaches the JS Engine (Transpiling is done by "Parcel"{It uses one of its package for transpiling} i.e BABLE)
+const jsxheading = <h1 id ="heading">"Namaste React using JSX 🚀"</h1> 
+
 console.log(jsxheading);
+// the console.log(heading) & console.log(jsxheading) is one of the samething both are JavaScripts objects 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(jsxheading);
