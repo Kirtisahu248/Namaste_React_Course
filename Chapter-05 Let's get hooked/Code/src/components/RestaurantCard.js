0b141ff477dd4b5d10e2116db185f9f7f0883dc2
Kirti Sahu <sahukirti1804@gmail.com>
2024-09-02 09:33:46 +0530

    ep-05 done

--- Chapter-05 Let's get hooked/Code/src/components/RestaurantCard.js
+++ Chapter-05 Let's get hooked/Code/src/components/RestaurantCard.js
@@ -15,7 +15,7 @@ const RestaurantCard = (props) => {
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating}⭐</h4>
+      <h4>{avgRating}❇️</h4>
       <h4>{costForTwo}</h4>
       <h4>{deliveryTime} mins</h4>
     </div>
